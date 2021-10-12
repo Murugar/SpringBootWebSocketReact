4c13f022d342014a24d78ab940646c2fb7b43ce5
Muruga <davanon2014@gmail.com>
2021-10-12 12:12:15 +0530

    First

--- src/main/java/com/iqmsoft/boot/ws/react/messages/dto/Greeting.java
+++ src/main/java/com/iqmsoft/boot/ws/react/messages/dto/Greeting.java
@@ -1,14 +1,17 @@
 package com.iqmsoft.boot.ws.react.messages.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class Greeting implements Serializable {
 
     private static final long serialVersionUID = 5242427420838566738L;
-    final String content;
+    String content;
 }
--- src/main/java/com/iqmsoft/boot/ws/react/messages/dto/HelloMessage.java
+++ src/main/java/com/iqmsoft/boot/ws/react/messages/dto/HelloMessage.java
@@ -1,14 +1,17 @@
 package com.iqmsoft.boot.ws.react.messages.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloMessage implements Serializable {
 
     private static final long serialVersionUID = 5166155550680045405L;
-    final String name;
+    String name;
 }
